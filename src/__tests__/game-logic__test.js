b4dbcff3a6185edcfaeafc73598eb16d82072372
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-11-09 08:28:47 +0000

    Game logic - When a game starts / At initial state - Should choose a random name within the list of first generation pokemon names

--- src/__tests__/game-logic__test.js
+++ src/__tests__/game-logic__test.js
@@ -1,59 +1,63 @@
 import gameLogic from "../services/game-logic";
+import pokemonList from "../constants/game-constants";
 
 describe("Game logic - ", () => {
   describe("When a game starts / At initial state - ", () => {
-    xit("Should choose a random name within the list of first generation pokemon names", () => {});
+    it("Should choose a random name within the list of first generation pokemon names", () => {
+      const chosenName = gameLogic.choseRandomPokemonName();
+      expect(pokemonList).toEqual(expect.arrayContaining([chosenName]))
+    });
 
-    xit("Should start with an initial number of 6 lives", () => {});
+    xit("Should start with an initial number of 6 lives", () => { });
 
-    xit("Should Know the state representation of the guessed word with underscores for each letter", () => {});
+    xit("Should Know the state representation of the guessed word with underscores for each letter", () => { });
   });
 
   describe("During a game - ", () => {
     describe("On a correct letter selection - ", () => {
-      xit("Should know that the chosen letter is correct", () => {});
+      xit("Should know that the chosen letter is correct", () => { });
 
-      xit("Should not update the number of lives left", () => {});
+      xit("Should not update the number of lives left", () => { });
 
-      xit("Should know the new state representation of the guessed word with correct replacements", () => {});
+      xit("Should know the new state representation of the guessed word with correct replacements", () => { });
     });
 
     describe("On an incorrect letter selection - ", () => {
-      xit("Should know that the chosen letter is incorrect", () => {});
+      xit("Should know that the chosen letter is incorrect", () => { });
 
-      xit("Should update the number of lives left by removing one", () => {});
+      xit("Should update the number of lives left by removing one", () => { });
 
-      xit("Should not update the state representation of the guessed word", () => {});
+      xit("Should not update the state representation of the guessed word", () => { });
     });
 
-    xit("Should not allow to select a letter twice", () => {});
+    xit("Should not allow to select a letter twice", () => { });
   });
 
   describe("When a game finishes - ", () => {
     describe("When the player won the game - ", () => {
-      xit("Should still has life/lives left", () => {});
+      xit("Should still has life/lives left", () => { });
 
-      xit("Should have a state representation of the guessed word equal to the chosen Pokemon name", () => {});
+      xit("Should have a state representation of the guessed word equal to the chosen Pokemon name", () => { });
 
-      xit("Should know that the player won", () => {});
+      xit("Should know that the player won", () => { });
     });
 
     describe("When the player lost the game - ", () => {
-      xit("Should have no life left", () => {});
+      xit("Should have no life left", () => { });
 
-      xit("Should have a state representation of the guessed word that differs from the chosen Pokemon name", () => {});
+      xit("Should have a state representation of the guessed word that differs from the chosen Pokemon name", () => { });
 
-      xit("Should know that the player lost", () => {});
+      xit("Should know that the player lost", () => { });
     });
   });
 
   describe("When a brand new game starts - ", () => {
-    xit("Should choose a new random name within the list of first generation pokemon names", () => {});
+    xit("Should choose a new random name within the list of first generation pokemon names", () => { });
 
-    xit("Should re-initialize the number of lives to 6", () => {});
+    xit("Should re-initialize the number of lives to 6", () => { });
 
-    xit("Should re-initialize the state representation of the guessed word with underscores for each letter", () => {});
+    xit("Should re-initialize the state representation of the guessed word with underscores for each letter", () => { });
 
-    xit("Should know that the game is not finished anymore (that the player has not won or lost anymore)", () => {});
+    xit("Should know that the game is not finished anymore (that the player has not won or lost anymore)", () => { });
   });
 });
--- src/services/game-logic.js
+++ src/services/game-logic.js
@@ -1,3 +1,8 @@
-const gameLogic = {};
+import pokemonList from "../constants/game-constants";
+import _ from 'lodash';
+
+const gameLogic = {
+  choseRandomPokemonName: () => _.sample(pokemonList)
+};
 
 export default gameLogic;
