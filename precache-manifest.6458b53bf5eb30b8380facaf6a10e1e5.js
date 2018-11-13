e7416a33859ac1f7b2a99ac407cfd2559eb32b8d
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-11-13 14:47:17 +0000

    Updates

---
+++ precache-manifest.6458b53bf5eb30b8380facaf6a10e1e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01ae1c3939b454b4ff9c",
+    "url": "/pokemon-hangman/static/js/runtime~main.01ae1c39.js"
+  },
+  {
+    "revision": "e21a18c1764ff80b9072",
+    "url": "/pokemon-hangman/static/js/main.e21a18c1.chunk.js"
+  },
+  {
+    "revision": "a766783f2b2751caf8cf",
+    "url": "/pokemon-hangman/static/js/1.a766783f.chunk.js"
+  },
+  {
+    "revision": "e21a18c1764ff80b9072",
+    "url": "/pokemon-hangman/static/css/main.e0bfd71a.chunk.css"
+  },
+  {
+    "revision": "9a0f085d76cc22080e8f6abe20bed896",
+    "url": "/pokemon-hangman/index.html"
+  }
+];
