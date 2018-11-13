f240ae7bb790fe0c7960d5edca38686a08f80768
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-11-13 13:34:52 +0000

    Update buttons rendering by adding its own state to it

--- src/components/molecules/buttons.js
+++ src/components/molecules/buttons.js
@@ -3,19 +3,51 @@ import PropTypes from 'prop-types';
 import { alphabetLetters } from "../../constants/game-constants";
 import Button from '../atoms/button';
 
-const Buttons = ({ onClick }) => alphabetLetters.map(letter => {
-  const button = (
-    <Button
-      className="App__alphabet-letter-button"
-      key={letter}
-      children={letter}
-      onClick={() => onClick(letter)} />
-  );
-  return button;
-});
+class Buttons extends React.Component {
+  constructor(props) {
+    super();
+    this.clickLetter = this.clickLetter.bind(this);
+    this.state = {
+      choices: []
+    };
+  }
+
+  clickLetter(letter) {
+    const newChoices = this.state.choices;
+    newChoices.push(letter);
+    this.setState({
+      choices: newChoices
+    });
+    this.props.onClick(letter)
+  }
+
+  render() {
+    return alphabetLetters.map(letter => {
+      const isButtonHidden = this.state.choices.includes(letter);
+
+      const button = (
+        <Button
+          className="App__alphabet-letter-button"
+          key={letter}
+          children={letter}
+          onClick={() => this.clickLetter(letter)} />
+      );
+
+      const hiddenButton = (letter) => (
+        <div
+          className="App__alphabet-letter-hidden-button"
+          key={letter}>
+          {letter}
+        </div>
+      );
+
+      return isButtonHidden ? hiddenButton(letter) : button;
+    })
+  };
+}
 
 Buttons.propTypes = {
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired
 };
 
 export default Buttons;
