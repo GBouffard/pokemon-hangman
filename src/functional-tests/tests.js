5d8d8b6da347acf91e2966bb3106b154e5ef6762
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-11-15 10:02:45 +0000

    Rewrite testcafe fixtures and first case of When playing

--- src/functional-tests/tests.js
+++ src/functional-tests/tests.js
@@ -1,32 +1,41 @@
 import { Selector } from 'testcafe';
 
-fixture('Guillaume Pokemon Hangman Functional Tests - ')
-  .page('https://gbouffard.github.io/pokemon-hangman/')
-  .beforeEach(async t => {
-    t.ctx.app = Selector('.App');
-    t.ctx.gameLogo = Selector('.App__logo');
-    // StatusImage will be tested as a React component
-    t.ctx.numberOfLives = Selector('.App__lives');
-    t.ctx.nameProgress = Selector('.App__name-progress');
-    t.ctx.aLetterButton = Selector('.App__alphabet-letter-button').nth(0);
-    t.ctx.bLetterButton = Selector('.App__alphabet-letter-button').nth(1);
-    t.ctx.newGameButton = Selector('.App__new-game-button');
-  });
+const app = Selector('.App');
+const gameLogo = Selector('.App__logo');
+// StatusImage will be tested as a React component
+const numberOfLives = Selector('.App__lives');
+const nameProgress = Selector('.App__name-progress');
+const aLetterButton = Selector('.App__alphabet-letter-button').nth(0);
+const bLetterButton = Selector('.App__alphabet-letter-button').nth(1);
+const newGameButton = Selector('.App__new-game-button');
+
+fixture('At initial state - ')
+  .page('https://gbouffard.github.io/pokemon-hangman/');
 
 test('Renders the page and all its initial components', async (t) => {
   await t
-    .expect(t.ctx.gameLogo.count).eql(1)
-    .expect(t.ctx.numberOfLives.exists).ok()
-    .expect(t.ctx.nameProgress.innerText).contains('CLICK NEW GAME')
-    .expect(t.ctx.newGameButton.count).eql(1)
-    .expect(t.ctx.app.innerText).contains('CLICK NEW GAME');
+    .expect(gameLogo.count).eql(1)
+    .expect(numberOfLives.exists).ok()
+    .expect(nameProgress.innerText).contains('CLICK NEW GAME')
+    .expect(newGameButton.count).eql(1)
+    .expect(app.innerText).contains('CLICK NEW GAME');
 });
 
-test('Does not show the alphabet buttons at initial state', async (t) => {
+test('Does not show the alphabet buttons', async (t) => {
   await t
-    .expect(t.ctx.aLetterButton.count).eql(0)
-    .expect(t.ctx.bLetterButton.exists).notOk();
+    .expect(aLetterButton.count).eql(0)
+    .expect(bLetterButton.exists).notOk();
 });
 
-test.skip('Starting a game', async (t) => {
+fixture('When playing - ')
+  .page('https://gbouffard.github.io/pokemon-hangman/');
+
+test('Starting a game update the views with the expected components', async (t) => {
+  await t
+    .click(newGameButton);
+
+  await t
+    .expect(app.innerText).notContains('CLICK NEW GAME')
+    .expect(aLetterButton.count).eql(1)
+    .expect(bLetterButton.exists).ok();
 });
